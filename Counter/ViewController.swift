3498e63af0290c89d735bf81530a7e9f062280d7
s4fonovoleg <s4fonovoleg@yandex.ru>
2023-02-19 21:39:22 +0300

    Добавлена кнопка и label с счетчиком

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	@IBOutlet weak var counterLabel: UILabel!
+	private let counterLabelTemplate = "Значение счетчика: "
+	private var count = 0
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
 
-
+	@IBAction func counterButtonDidTap() {
+		count += 1
+		counterLabel.text = "\(counterLabelTemplate)\(count)"
+	}
 }
-
